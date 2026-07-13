305f70a12fd5d05a4cea7f2fb2f20991e19301e3
Deniz Çelik <276564141+denizcelik0@users.noreply.github.com>
2026-07-13 15:33:22 +0300

    Check user adult or minor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 name  = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
-print(f"Hello, {name}!")
-print(f"You are {age} years old.")
-print(f"Next year, you will be {age + 1} years old. ")
+if age >= 18:
+    print(f"Hello {name}, you are an adult.")
+else:
+    print(f"Hello {name}, you are a minor.")
     
