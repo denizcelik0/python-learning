4606f34921748fffe7bfb40e5f3c47dd171854a4
Deniz Çelik <276564141+denizcelik0@users.noreply.github.com>
2026-07-13 15:37:04 +0300

    Score check

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-name  = input("Enter your name: ")
-age = int(input("Enter your age: "))
+score = int(input("Enter your score: "))
 
-if age >= 18:
-    print(f"Hello {name}, you are an adult.")
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")   
+elif score >= 60:
+    print("Grade: D")
 else:
-    print(f"Hello {name}, you are a minor.")
+    print("Grade: F")
+
     
+            
