c97f74f913cf489b49ec03d56b0ce9b90d8b6b99
Deniz Çelik <276564141+denizcelik0@users.noreply.github.com>
2026-07-13 15:28:40 +0300

    Add user input f-strings

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, World!")
-print(2026)
-print(19+20)
+name  = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello, {name}!")
+print(f"You are {age} years old.")
+print(f"Next year, you will be {age + 1} years old. ")
+    
